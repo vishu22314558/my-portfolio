c7cbf60397770368c852116236a1c5bbe66f4b10
Vishva jeet Singh <vishuwish.singh@gmail.com>
2019-06-20 06:39:36 -0500

    added boto3 code

---
+++ upload.py
@@ -0,0 +1,29 @@
+import boto3
+from botocore.client import Config
+import zipfile
+import mimetypes
+import io
+from io import BytesIO
+
+
+#Create S3 object 
+
+s3 = boto3.resource('s3', config = Config(signature_version = 's3v4'))
+
+# Create Bucekt Objects 
+
+portfolio_bucket = s3.Bucket('portfolio.svadhyaya.info')
+build_bucket = s3.Bucket('portfoliobuild.svadhyaya.info')
+
+#portfolio_zip = io.StringIO()
+portfolio_zip = BytesIO()
+
+build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm , ExtraArgs = {'ContentType' : mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL = 'public-read')
+
+
