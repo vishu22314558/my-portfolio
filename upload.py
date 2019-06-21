b9a2dfa39b02f82e0fb060db11371a43058a3b0c
Vishva jeet Singh <vishuwish.singh@gmail.com>
2019-06-20 23:01:53 -0500

    updated

--- upload.py
+++ upload.py
@@ -5,29 +5,38 @@ import zipfile
 import mimetypes
 import io
 from io import BytesIO
+#SNS
+
 
 def lambda_handler(event, context):
     
     #Create S3 object 
-    
-    s3 = boto3.resource('s3', config = Config(signature_version = 's3v4'))
-    
-    # Create Bucekt Objects 
-    
-    portfolio_bucket = s3.Bucket('portfolio.svadhyaya.info')
-    build_bucket = s3.Bucket('portfoliobuild.svadhyaya.info')
-    
-    #portfolio_zip = io.StringIO()
-    portfolio_zip = BytesIO()
-    
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm , ExtraArgs = {'ContentType' : mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL = 'public-read')
-    
+    try:
+        s3 = boto3.resource('s3', config = Config(signature_version = 's3v4'))
+        sns = boto3.resource('sns')
+
+        topic = sns.Topic('arn:aws:sns:us-east-1:244691138631:deployPortfolioTopic')
+
+        # Create Bucekt Objects 
+        
+        portfolio_bucket = s3.Bucket('portfolio.svadhyaya.info')
+        build_bucket = s3.Bucket('portfoliobuild.svadhyaya.info')
+        
+        #portfolio_zip = io.StringIO()
+        portfolio_zip = BytesIO()
+        
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm , ExtraArgs = {'ContentType' : mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL = 'public-read')
+        
+        topic.publish(Subject = "Updated Portfolio" , Message="We have deployed yout Portfolio")
+    except:
+        topic.publish(Subject = "Updated Portfolio" , Message="Deployment Failed")    
+        raise
     
     return {
         'statusCode': 200,
