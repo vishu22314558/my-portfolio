9df400907805272f8c37e8718a5d38af75b680ae
Vishva jeet Singh <vishuwish.singh@gmail.com>
2019-07-13 17:13:27 -0500

    v5

--- upload_local.py
+++ upload_local.py
@@ -4,14 +4,15 @@ import zipfile
 
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-build_bucket = s3.Bucket('portfoliobuild.robinnorwood.info')
-portfolio_bucket = s3.Bucket('portfolio.robinnorwood.info')
+portfolio_bucket = s3.Bucket('portfolio.svadhyaya.info')
+
+build_bucket = s3.Bucket('portfoliobuild.svadhyaya.info')
 
 # On Windows, this will need to be a different location than /tmp
-build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
+build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
 
-with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
+with zipfile.ZipFile('/tmp/portfoliobuild.zip') as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        target_bucket.upload_fileobj(obj, nm)
-        target_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_bucket.upload_fileobj(obj, nm , ExtraArgs = {'ContentType' : mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
